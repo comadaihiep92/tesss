b771f113dbdb886d624edec0211cbb9676d16602
comadaihiep92 <motden100@gmail.com>
2020-04-29 16:15:54 +0700

    add file new

---
+++ src/components/layout3.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Menu from "./menu-cloud"
+import Menu2 from "./menu2"
+
+const Layout3 = ({ children }) => {
+  return (
+    <div className="app">
+      <Menu2 />
+      <div className="wrapper">
+        <Menu />
+        <div className="cd-hero">{children}</div>
+        <footer className="footer">
+          <div>© 2020 by Stork. All rights reserved.</div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Layout3
